9f520bd8214173cb7caf40d8d192e2c1b90f5736
imrish-18 <rish.sharma9012@gmail.com>
2021-05-25 11:47:11 +0530

    commit

--- src/using_Java/LinkedList/LinkedList.java
+++ src/using_Java/LinkedList/LinkedList.java
@@ -1,5 +1,7 @@
 package LinkedList;
 import java.util.*;
+
+import LinkedList.LinkedList.Node;
 public class LinkedList {
 	Node head;
 	static class Node
@@ -119,7 +121,7 @@ public class LinkedList {
        int data=0;
        System.out.println("enter the data into list");
        
-       for(int i=0;i<4;i++)
+       for(int i=0;i<5;i++)
        {
     	    data=sc.nextInt();
     	   insertNode(data, list);
@@ -130,6 +132,8 @@ public class LinkedList {
 //       data=sc.nextInt();
        //insertLastNode(data, list);
        
+     Node mid= mergeSort(list.head);
+     System.out.println(mid.data);
        System.out.println("the data into linked list are..");
        traverse(list.head);
        
@@ -146,6 +150,121 @@ public class LinkedList {
 //       
 //       sc.close();
        }
+
+//	
+//	  public static Node merge(Node nxt)
+//	  {
+//		  if(nxt.next==null || nxt==null)
+//			  return nxt;
+//		  
+//		  // middle of the node
+//		  Node middle =getMiddle(nxt);
+//		  
+//		  Node nextofmiddle=middle.next;
+//		  middle.next=null;
+//		  // Apply mergeSort on left list 
+//	        Node left = merge(nxt); 
+//	  
+//	        // Apply mergeSort on right list 
+//	        Node right = merge(nextofmiddle); 
+//	        Node sortedlist = sortedMerge(left, right); 
+//	        return sortedlist; 
+//		 
+//	  }
+//	  
+//	  public static Node sortedMerge(Node a,Node b)
+//	  {
+//		  Node res=null;
+//		  if(a==null)
+//			  return b;
+//		  if(b==null)
+//			  return a;
+//		  
+//		  if(a.data<=b.data)
+//		  {
+//			  res=a;
+//			  res.next=sortedMerge(a.next,b );
+//			  
+//		  }
+//		  else
+//		  {
+//			  res=b;
+//			  res.next=sortedMerge(a,b.next );
+//			  
+//		  }
+//		  return res;
+//	  }
+//	  public static Node getMiddle(Node ptr)
+//	  {
+//		  if(ptr==null)
+//			  return ptr;
+//		  Node a=ptr;
+//		  Node  b=ptr;
+//		  while(b.next!=null && b.next.next!=null)
+//		  {
+//			  a=a.next;
+//			  b=b.next.next;
+//		  }
+//		  return a;
+//	  }
+	public static Node mergeSort(Node h)
+	{
+		 if (h == null || h.next == null) { 
+	            return h; 
+	        } 
+	  
+	        // get the middle of the list 
+	        Node middle = middleNode(h); 
+	        Node nextofmiddle = middle.next; 
+	  
+	        // set the next of middle node to null 
+	        middle.next = null; 
+	  
+	        // Apply mergeSort on left list 
+	        Node left = mergeSort(h); 
+	  
+	        // Apply mergeSort on right list 
+	        Node right = mergeSort(nextofmiddle); 
+	  
+	        // Merge the left and right lists 
+	        Node sortedlist = sortedMerge(left, right); 
+	        return sortedlist; 
+	}
+	
+	
+	public static Node sortedMerge(Node a,Node b) {
+		Node result = null; 
+        /* Base cases */
+        if (a == null) 
+            return b; 
+        if (b == null) 
+            return a; 
+  
+        /* Pick either a or b, and recur */
+        if (a.data <= b.data) { 
+            result = a; 
+            result.next = sortedMerge(a.next, b); 
+        } 
+        else { 
+            result = b; 
+            result.next = sortedMerge(a, b.next); 
+        } 
+        return result; 
+	}
+	
+	public static Node middleNode(Node node)
+	{
+		if(node==null)
+			return node;
+		Node a=node;
+		Node b=node;
+		while(b.next!=null && b.next.next!=null)
+		{
+			a=a.next;
+			b=b.next.next;
+		}
+		return a;
+	}
 
 	}
 
